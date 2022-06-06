5adc08ba6e8217945697987c40b0c0d510765af5
Eric Smith <erwsmith@gmail.com>
2022-06-06 16:08:43 -0600

    csv creation added to yahoo, management.py initial

--- yahoo.py
+++ yahoo.py
@@ -1,43 +1,41 @@
 from yahoofinancials import YahooFinancials
 import pandas as pd
-import matplotlib.pyplot as plt
 import numpy as np
 
 ticker = 'UFI'
 yf = YahooFinancials(ticker)
 
-# income statement data
-# income_statement = yf.get_financial_stmts('annual', 'income')
-# ticker_is = income_statement['incomeStatementHistory'][ticker]
-
-
-# cash flow data
-# cash_flow = yf.get_financial_stmts('annual', 'cash')
-# ticker_cf = cash_flow['cashflowStatementHistory'][ticker]
-
-
+# BALANCE STATEMENT DATA
 # get balance statement data from yahoo finance
 balance_statement = yf.get_financial_stmts('annual', 'balance')
 
 # get balance sheet history from statement
 ticker_bs = balance_statement['balanceSheetHistory'][ticker]
 
-# create list for balance sheet history 
-df_list = []
+# create and populate list of balance sheet history 
+balance_sheet_list = []
 for d in ticker_bs:
-    df_list.append(pd.DataFrame.from_dict(d, orient='index'))
-df_bs = pd.concat(df_list)
-# print(df_bs)
+    balance_sheet_list.append(pd.DataFrame.from_dict(d, orient='index'))
+bs_table = pd.concat(balance_sheet_list)
 
 # save balance statement data to csv
-# df_bs.to_csv("balance_statment.csv")
+bs_table.to_csv("balance_statment.csv")
 
-# calculate de ratio and create new column for it
-df_bs['de_ratio'] = df_bs['totalLiab'] / df_bs['totalStockholderEquity']
 
-# transpose de ratio column
-col = df_bs['de_ratio'].agg('mean').transpose()
+# INCOME STATEMENT DATA
+income_statement = yf.get_financial_stmts('annual', 'income')
+ticker_is = income_statement['incomeStatementHistory'][ticker]
+income_statement_list = []
+for d in ticker_is:
+    income_statement_list.append(pd.DataFrame.from_dict(d, orient='index'))
+is_table = pd.concat(income_statement_list)
+is_table.to_csv("income_statment.csv")
 
-# create pass/fail bool for acceptable de ratio
-result = (col < 5) & (col > 0)
-# print(result)
+# CASH FLOW DATA
+cash_flow = yf.get_financial_stmts('annual', 'cash')
+ticker_cf = cash_flow['cashflowStatementHistory'][ticker]
+cash_flow_list = []
+for d in ticker_cf:
+    cash_flow_list.append(pd.DataFrame.from_dict(d, orient='index'))
+cf_table = pd.concat(cash_flow_list)
+cf_table.to_csv("cash_flow.csv")
