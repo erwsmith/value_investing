0afe82c7d555b82b7ace0ff068b52e168b8b4f88
Eric Smith <erwsmith@gmail.com>
2022-06-09 16:28:22 -0600

    removed all yahoo stuff

--- helpers.py
+++ helpers.py
@@ -6,7 +6,6 @@ import json
 
 from flask import redirect, render_template, request, session
 from functools import wraps
-from yahoofinancials import YahooFinancials
 
 
 def apology(message, code=400):
@@ -99,49 +98,3 @@ def lookup_income_statement(symbol):
 def usd(value):
     """Format value as USD."""
     return f"${value:,.2f}"
-
-
-def yahoo(ticker):
-    """Get data for symbol."""
-
-    yf = YahooFinancials(ticker)
-
-    # BALANCE STATEMENT DATA
-    # get balance statement data from yahoo finance
-    balance_statement = yf.get_financial_stmts('annual', 'balance')
-
-    # get balance sheet history from statement
-    ticker_bs = balance_statement['balanceSheetHistory'][ticker]
-
-    # create and populate list of balance sheet history 
-    balance_sheet_list = []
-    for d in ticker_bs:
-        balance_sheet_list.append(pd.DataFrame.from_dict(d, orient='index'))
-    bs_table = pd.concat(balance_sheet_list)
-
-    # save balance statement data to csv
-    bs_table.to_csv("balance_statment.csv")
-
-
-    # INCOME STATEMENT DATA
-    income_statement = yf.get_financial_stmts('annual', 'income')
-    ticker_is = income_statement['incomeStatementHistory'][ticker]
-    income_statement_list = []
-    for d in ticker_is:
-        income_statement_list.append(pd.DataFrame.from_dict(d, orient='index'))
-    is_table = pd.concat(income_statement_list)
-    is_table.to_csv("income_statment.csv")
-
-
-    # CASH FLOW DATA
-    cash_flow = yf.get_financial_stmts('annual', 'cash')
-    ticker_cf = cash_flow['cashflowStatementHistory'][ticker]
-    cash_flow_list = []
-    for d in ticker_cf:
-        cash_flow_list.append(pd.DataFrame.from_dict(d, orient='index'))
-    cf_table = pd.concat(cash_flow_list)
-    cf_table.to_csv("cash_flow.csv")
-
-
-def management():
-    return None
