be4c90655197756c34b8c607fba227b78122071d
Eric Smith <erwsmith@gmail.com>
2022-06-09 17:37:04 -0600

    paring down df to desired values only

--- read_balance_sheet.py
+++ read_balance_sheet.py
@@ -11,32 +11,11 @@ def read_balance_sheet(sym):
     balance_sheet = json.loads(data)
     annual_reports = balance_sheet["annualReports"]
     df = pd.json_normalize(annual_reports)
+    df.set_index("fiscalDateEnding", inplace=True)
+    df = df[["totalLiabilities", "totalShareholderEquity", "totalCurrentLiabilities", "totalCurrentAssets", "longTermDebt"]]
+    # df = df[["totalLiabilities", "totalShareholderEquity", "totalCurrentLiabilities", "totalCurrentAssets"]]
+    df = df.astype('float')
+    df["de_ratio"] = df["totalLiabilities"] / df["totalShareholderEquity"]
     print(df)
-
-# for report in annual_reports:
-#     print(report["fiscalDateEnding"], report["totalLiabilities"])
-
-
-# return {
-#     "totalLiabilities": balance_sheet["totalLiabilities"],
-#     "totalShareholderEquity": balance_sheet["totalShareholderEquity"],
-#     "totalCurrentLiabilities": balance_sheet["totalCurrentLiabilities"],
-#     "totalCurrentAssets": balance_sheet["totalCurrentAssets"],
-#     "longTermDebt": balance_sheet["longTermDebt"]
-# }
-
-
-# # Parse response
-#     try:
-#         balance_sheet = response.json()
-#         return {
-#             "totalLiabilities": balance_sheet["totalLiabilities"],
-#             "totalShareholderEquity": balance_sheet["totalShareholderEquity"],
-#             "totalCurrentLiabilities": balance_sheet["totalCurrentLiabilities"],
-#             "totalCurrentAssets": balance_sheet["totalCurrentAssets"],
-#             "longTermDebt": balance_sheet["longTermDebt"]
-#         }
-#     except (KeyError, TypeError, ValueError):
-#         return None
 
 read_balance_sheet("UFI")
