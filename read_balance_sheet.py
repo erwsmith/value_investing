2f4ce8b9ab01a7db0a07db381369972fefc25d9e
Eric Smith <erwsmith@gmail.com>
2022-06-10 11:56:55 -0600

    added current ratio calculation

--- read_balance_sheet.py
+++ read_balance_sheet.py
@@ -2,6 +2,12 @@ import json
 import pandas as pd
 import numpy as np
 
+# Balance sheet numbers for management: 
+# DEBT TO EQUITY RATIO
+# TOTAL LIABILITIES / TOTAL SHAREHOLDER EQUITY
+
+# CURRENT RATIO
+# CURRENT ASSETS / CURRENT LIABILITIES
 
 def read_balance_sheet(sym):
     filepath = f"json_files/balance_sheet_{sym}.json"
@@ -13,9 +19,9 @@ def read_balance_sheet(sym):
     df = pd.json_normalize(annual_reports)
     df.set_index("fiscalDateEnding", inplace=True)
     df = df[["totalLiabilities", "totalShareholderEquity", "totalCurrentLiabilities", "totalCurrentAssets", "longTermDebt"]]
-    # df = df[["totalLiabilities", "totalShareholderEquity", "totalCurrentLiabilities", "totalCurrentAssets"]]
     df = df.astype('float')
     df["de_ratio"] = df["totalLiabilities"] / df["totalShareholderEquity"]
+    df["current_ratio"] = df["totalCurrentAssets"] / df["longTermDebt"]
     print(df)
 
 read_balance_sheet("UFI")
