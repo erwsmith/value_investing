2a0e87abee1b137018eb2b5e45b5c5b9cc8fff96
Eric Smith <erwsmith@gmail.com>
2022-06-13 14:48:45 -0600

    added *some* roic calc numbers, roic not finished yet

--- read_jsons.py
+++ read_jsons.py
@@ -19,24 +19,28 @@ def read_jsons(sym):
 
     # BALANCE SHEET - read and setup dataframe
     bs_filepath = f"json_files/balance_sheet_{sym}.json"
-    with open(bs_filepath, "r") as f: 
+    with open(bs_filepath, "r+") as f:
         data = f.read()
 
     # Process data and create clean dataframe
     balance_sheet = json.loads(data)
     annual_reports = balance_sheet["annualReports"]
+    print(annual_reports)
     df_balance = pd.json_normalize(annual_reports)
     df_balance["fiscalDateEnding"] = pd.to_datetime(df_balance["fiscalDateEnding"])
     df_balance['year'] = pd.DatetimeIndex(df_balance['fiscalDateEnding']).year
     df_balance.set_index("year", inplace=True)
+    # print(df_balance)
 
     # Reduce df to have only required columns, cast values as float
-    df_balance = df_balance[["totalLiabilities", "totalShareholderEquity", "totalCurrentLiabilities", "totalCurrentAssets", "longTermDebt"]]
+    df_balance.replace("None", "0", inplace=True)
+    df_balance = df_balance[["totalLiabilities", "totalShareholderEquity", "totalCurrentLiabilities", "totalCurrentAssets", "longTermDebt", "capitalLeaseObligations"]]
     df_balance = df_balance.astype('float')
 
     # Calculate de and current ratios:
     df_balance["de_ratio"] = df_balance["totalLiabilities"] / df_balance["totalShareholderEquity"]
     df_balance["current_ratio"] = df_balance["totalCurrentAssets"] / df_balance["longTermDebt"]
+    df_balance["invested_capital"] = df_balance["totalShareholderEquity"] + df_balance["longTermDebt"] + df_balance["capitalLeaseObligations"]
 
     # CASH FLOW STATEMENT - read and setup dataframe
     cf_filepath = f"json_files/cash_flow_{sym}.json"
@@ -52,11 +56,13 @@ def read_jsons(sym):
     df_cash.set_index("year", inplace=True)
 
     # Reduce df to have only required columns, cast values as float
+    df_cash.replace("None", "0", inplace=True)
     df_cash = df_cash[["operatingCashflow", "capitalExpenditures"]]
     df_cash = df_cash.astype('float')
 
     # Calculate freeCashFlow
     df_cash["freeCashFlow"] = df_cash["operatingCashflow"] - df_cash["capitalExpenditures"]
+    # print(df_cash)
     
 
     # INCOME STATEMENT - read and setup dataframe
@@ -72,20 +78,33 @@ def read_jsons(sym):
     df_income['year'] = pd.DatetimeIndex(df_income['fiscalDateEnding']).year
     df_income.set_index("year", inplace=True)
 
-    # TODO Reduce df to have only required columns, cast values as float
-    # df_income = df_income[["", ""]]
-    # df_income = df_income.astype('float')
-    print(df_income)
+    # Reduce df to have only required columns, cast values as float
+    df_income.replace("None", "0", inplace=True)
+    df_income = df_income[["ebit", "incomeTaxExpense", "netIncome"]]
+    df_income = df_income.astype('float')
+
+    # Calculate effective tax rate
+    # effective_tax_rate = incomeTaxExpense / netIncome
+    df_income["effectiveTaxRate"] = df_income["incomeTaxExpense"] / df_income["netIncome"]
+
+    # Calculate NOPAT
+    # nopat = ebit * (1 - effective_tax_rate)
+    df_income["nopat"] = df_income["ebit"] * (1 - df_income["effectiveTaxRate"])
+    # print(df_income)
     
     # COMBINE DATAFRAMES
-    df = df_balance[["de_ratio", "current_ratio", "longTermDebt"]]
-    df = df.join(df_cash[["freeCashFlow"]])
+    df = df_balance[["de_ratio", "current_ratio", "longTermDebt", "invested_capital"]]
+    df = df.join(df_cash[["freeCashFlow"]]).join(df_income[["nopat"]])
 
     # Calculate durability
     df["durability"] = df["longTermDebt"] / df["freeCashFlow"]
 
+    # Calculate ROIC
+    df["roic"] = df["nopat"] / df["invested_capital"]
+
     # Set final columns
-    df = df[["de_ratio", "current_ratio", "durability"]]
+    df = df[["de_ratio", "current_ratio", "durability", "roic"]]
+
 
     # Sort by year, transpose, and calculate 5 year average values
     df = df.sort_index().transpose()
@@ -115,5 +134,6 @@ def read_jsons(sym):
     if df.loc["durability","pass"] and df.loc["current_ratio", "pass"] and df.loc["de_ratio", "pass"]:
         management_check = True
 
+    # print(df)
 
 read_jsons("UFI")
