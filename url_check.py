5baaed0c234c6a7480c414110c37e432dfe002f1
Eric Smith <erwsmith@gmail.com>
2022-06-15 13:58:35 -0600

    testing & development of lookup consolidation

--- url_check.py
+++ url_check.py
@@ -4,114 +4,22 @@ import requests
 import json
 
 """
-testing for successful lookup and json file creation for balance sheet, cash flow, and income statements.
+testing for successful requests to alphavantage and json file creation of 5 main functions
+lookup_functions = ["BALANCE_SHEET", "CASH_FLOW", "INCOME_STATEMENT", "GLOBAL_QUOTE", "OVERVIEW"]
 """
 
-def lookup_balance_sheet(symbol):
-    try: 
-        api_key = os.environ.get("API_KEY")
-        sym = urllib.parse.quote_plus(symbol)
-        func = "BALANCE_SHEET"
-        url = f"https://www.alphavantage.co/query?function={func}&symbol={sym}&apikey={api_key}"
-        response = requests.get(url)
-        response.raise_for_status()
-        balance_sheet = response.json()
-        filepath = f"json_files/balance_sheet_{sym}.json"
-        with open(filepath, "w", encoding="utf-8") as f:
-            json.dump(balance_sheet, f, ensure_ascii=False, indent=4)
-
-    except requests.RequestException:
-        return None
-
-
-def lookup_cash_flow(symbol):
-    try:
-        api_key = os.environ.get("API_KEY")
-        sym = urllib.parse.quote_plus(symbol)
-        func = "CASH_FLOW"
-        url = f"https://www.alphavantage.co/query?function={func}&symbol={sym}&apikey={api_key}"
-        response = requests.get(url)
-        response.raise_for_status()
-        cash_flow = response.json()
-        filepath = f"json_files/cash_flow_{sym}.json"
-        with open(filepath, "w", encoding="utf-8") as f:
-            json.dump(cash_flow, f, ensure_ascii=False, indent=4)
-
-    except requests.RequestException:
-        return None
-
-
-def lookup_income_statement(symbol):
-    """
-    lookup income statement data for last 5 years and save as .json file
-    """
-    try:
-        api_key = os.environ.get("API_KEY")
-        sym = urllib.parse.quote_plus(symbol)
-        func = "INCOME_STATEMENT"
-        url = f"https://www.alphavantage.co/query?function={func}&symbol={sym}&apikey={api_key}"
-        response = requests.get(url)
-        response.raise_for_status()
-        income_statement = response.json()
-        filepath = f"json_files/income_statement_{sym}.json"
-        with open(filepath, "w", encoding="utf-8") as f:
-            json.dump(income_statement, f, ensure_ascii=False, indent=4)
-
-    except requests.RequestException:
-        return None
-
-
-def lookup_earnings(symbol):
-    """
-    lookup EPS history and save as .json file
-    """
-    try:
-        api_key = os.environ.get("API_KEY")
-        sym = urllib.parse.quote_plus(symbol)
-        func = "EARNINGS"
-        url = f"https://www.alphavantage.co/query?function={func}&symbol={sym}&apikey={api_key}"
-        response = requests.get(url)
-        response.raise_for_status()
-        earnings = response.json()
-        filepath = f"json_files/earnings_{sym}.json"
-        with open(filepath, "w", encoding="utf-8") as f:
-            json.dump(earnings, f, ensure_ascii=False, indent=4)
-
-    except requests.RequestException:
-        return None
-
-
-def lookup_quote(symbol):
-    """
-    lookup price history and save as .json file
-    """
-    try:
-        api_key = os.environ.get("API_KEY")
-        sym = urllib.parse.quote_plus(symbol)
-        func = "GLOBAL_QUOTE"
-        url = f"https://www.alphavantage.co/query?function={func}&symbol={sym}&apikey={api_key}"
-        response = requests.get(url)
-        response.raise_for_status()
-        quote = response.json()
-        filepath = f"json_files/quote_{sym}.json"
-        with open(filepath, "w", encoding="utf-8") as f:
-            json.dump(quote, f, ensure_ascii=False, indent=4)
-
-    except requests.RequestException:
-        return None
-
-def lookup(symbol, func):
+def lookup(sym, func):
     """
     request api function and save as .json file
     """
     try:
         api_key = os.environ.get("API_KEY")
-        sym = urllib.parse.quote_plus(symbol)
+        sym = urllib.parse.quote_plus(sym)
         url = f"https://www.alphavantage.co/query?function={func}&symbol={sym}&apikey={api_key}"
         response = requests.get(url)
         response.raise_for_status()
         data = response.json()
-        filepath = f"json_files/{func}_{sym}.json"
+        filepath = f"json_files/{sym}_{func}.json"
         with open(filepath, "w", encoding="utf-8") as f:
             json.dump(data, f, ensure_ascii=False, indent=4)
 
@@ -120,9 +28,14 @@ def lookup(symbol, func):
 
 
 sym = "LRCX"
-# lookup_balance_sheet(sym)
-# lookup_cash_flow(sym)
-# lookup_income_statement(sym)
-# lookup_earnings(sym)
-# lookup_quote(sym)
-lookup(sym, "OVERVIEW")
+lookup_functions = ["BALANCE_SHEET", "CASH_FLOW", "INCOME_STATEMENT", "OVERVIEW", "GLOBAL_QUOTE"]
+for func in lookup_functions:
+    lookup(sym, func)
+
+# IF MORE ALPHAVANTAGE REQUESTS NEED TO BE MADE:
+# Wait 1 minute after doing 5 requests
+# import time
+# time.sleep(60)
+# lookup_functions = [""]
+# for func in lookup_functions:
+#     lookup(sym, func)
