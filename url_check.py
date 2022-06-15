92d32ced4adefa8bdf11e2df128e4c5469883df4
Eric Smith <erwsmith@gmail.com>
2022-06-15 11:20:57 -0600

    added lookup_quote and general lookup, last commit before putting all "lookups" into single function

--- url_check.py
+++ url_check.py
@@ -81,8 +81,48 @@ def lookup_earnings(symbol):
         return None
 
 
+def lookup_quote(symbol):
+    """
+    lookup price history and save as .json file
+    """
+    try:
+        api_key = os.environ.get("API_KEY")
+        sym = urllib.parse.quote_plus(symbol)
+        func = "GLOBAL_QUOTE"
+        url = f"https://www.alphavantage.co/query?function={func}&symbol={sym}&apikey={api_key}"
+        response = requests.get(url)
+        response.raise_for_status()
+        quote = response.json()
+        filepath = f"json_files/quote_{sym}.json"
+        with open(filepath, "w", encoding="utf-8") as f:
+            json.dump(quote, f, ensure_ascii=False, indent=4)
+
+    except requests.RequestException:
+        return None
+
+def lookup(symbol, func):
+    """
+    request api function and save as .json file
+    """
+    try:
+        api_key = os.environ.get("API_KEY")
+        sym = urllib.parse.quote_plus(symbol)
+        url = f"https://www.alphavantage.co/query?function={func}&symbol={sym}&apikey={api_key}"
+        response = requests.get(url)
+        response.raise_for_status()
+        data = response.json()
+        filepath = f"json_files/{func}_{sym}.json"
+        with open(filepath, "w", encoding="utf-8") as f:
+            json.dump(data, f, ensure_ascii=False, indent=4)
+
+    except requests.RequestException:
+        return None
+
+
 sym = "LRCX"
 # lookup_balance_sheet(sym)
 # lookup_cash_flow(sym)
 # lookup_income_statement(sym)
-lookup_earnings(sym)
+# lookup_earnings(sym)
+# lookup_quote(sym)
+lookup(sym, "OVERVIEW")
