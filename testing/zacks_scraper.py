1bdcecc4ce42b723ef214d4b6651834606ea2e43
Eric Smith <erwsmith@gmail.com>
2022-06-16 15:09:19 -0600

    more testing, no response from zacks, likely blocked

--- testing/zacks_scraper.py
+++ testing/zacks_scraper.py
@@ -1,17 +1,19 @@
+import os
 import requests
 from symtable import SymbolTable
 import requests
 import urllib.parse
+import bs4
 
-def scrape_zacks(sym):
-    url = f"https://www.zacks.com/stock/quote/{sym}/detailed-earning-estimates"
-    print(url)
+def scrape_zacks():
+    # url = f"https://www.zacks.com/stock/quote/{sym}/detailed-earning-estimates"
+    url = "https://books.toscrape.com/"    
+    response = requests.get(url)
+    data = response.json()
+    print(data)
+    # print(response)
+    # soup = bs4.BeautifulSoup(response.text,'lxml')
+    # print(soup)
 
 # scrape_zacks("LRCX")
-
-
-def urllib(sym):
-    sym = urllib.parse.quote_plus(SymbolTable)
-    print(sym)
-
-urllib("LRCX")
+scrape_zacks()
