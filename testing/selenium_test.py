3cf9640784aafbdd22b1ec99d92600b277841eb2
Eric Smith <erwsmith@gmail.com>
2022-06-21 10:40:22 -0600

    got selenium test working with yahoo finance!

--- testing/selenium_test.py
+++ testing/selenium_test.py
@@ -6,9 +6,23 @@ from selenium.webdriver.support.select import Select
 import time
 
 path = "/usr/local/bin"
-driver = webdriver.Chrome()
+options = Options()
+options.headless = True
+options.add_argument("--window-size=192,120")
+driver = webdriver.Chrome(options=options)
 
-# # google search working example
+# yahoo finance
+sym = "LRCX"
+url = f"https://finance.yahoo.com/quote/LRCX/analysis?p={sym}"
+driver.get(url)
+full_xpath = "/html/body/div[1]/div/div/div[1]/div/div[3]/div[1]/div/div[1]/div/div/section/table[6]/tbody/tr[5]/td[2]"
+element = driver.find_element(by=By.XPATH, value=full_xpath)
+time.sleep(1)
+print(element.text)
+driver.close()
+
+
+# google search working example
 # driver.get("https://www.google.com")
 # time.sleep(1)
 # search_box = driver.find_element_by_name("q")
@@ -17,7 +31,7 @@ driver = webdriver.Chrome()
 # time.sleep(2)
 # driver.close()
 
-# # books.toscrape webscraping sandbox example
+# books.toscrape webscraping sandbox working example
 # url = "https://books.toscrape.com/"
 # driver.get(url)
 # elements = driver.find_elements(By.CLASS_NAME, "price_color")
@@ -27,13 +41,9 @@ driver = webdriver.Chrome()
 # print(e_list)
 # driver.close()
 
-# # non-functioning stub for yahoo finance
-# sym = "LRCX"
-# url = f"https://finance.yahoo.com/quote/LRCX/analysis?p={sym}"
+# books.toscrape webscraping sandbox another working example, using full XPATH
+# url = "https://books.toscrape.com/"
 # driver.get(url)
-# select_element = driver.find_element(By.CLASS_NAME, "Ta(end) Py(10px)")
-# select_object = Select(select_element)
-# select_object.select_by_index(1)
-# all_selected_options = select_object.all_selected_options
-# print(all_selected_options)
+# element = driver.find_element(by=By.XPATH, value="/html/body/div/div/div/div/section/div[2]/ol/li[2]/article/div[2]/p[1]")
+# print(element.text)
 # driver.close()
