955817ddc1dee7263c876c9684c6bb0560682c16
Eric Smith <erwsmith@gmail.com>
2022-06-23 10:50:45 -0600

    commented out sql db identification line

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///finance.db")
+# db = SQL("sqlite:///finance.db")
 
 # Make sure API key is set
 if not os.environ.get("API_KEY"):
